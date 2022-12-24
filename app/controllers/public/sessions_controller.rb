c46dde0fc50d5b2d2cdc9df9830f1fb6a40ec094
urotansh <114655455+urotansh@users.noreply.github.com>
2022-12-24 18:27:07 +0000

    [Add] ユーザー機能 編集画面の実装

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
 class Public::SessionsController < Devise::SessionsController
+  
+  def after_sign_in_path_for(resource)
+    user_path(current_user.name)
+  end
+  
   def guest_sign_in
     user = User.guest
     sign_in user
