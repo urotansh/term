84cb13c6938d98af6aa4058bda0851f42e00ccc8
urotansh <114655455+urotansh@users.noreply.github.com>
2022-12-25 14:49:47 +0000

    [Add] 管理者機能 ユーザー情報更新機能の実装

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -6,7 +6,25 @@ class Admin::UsersController < ApplicationController
   def edit
     @user = User.find_by(name: params[:name])
   end
-
+  
+  def update
+    @user = User.find_by(name: params[:name])
+    if @user.update(user_params)
+      redirect_to edit_admin_user_path(@user.name), notice: "ユーザー情報を更新しました。"
+    else
+      # レシーバのユーザ名が不正値でupdateされるため正常値へ戻す
+      @user.name = params[:name]
+      render :edit
+    end
+  end
+  
   def show
   end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :email, :is_deleted, :image)
+  end
+  
 end
