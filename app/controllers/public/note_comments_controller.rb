561c549cd6c0d4d92c7732dc7e9bb5d8e3fd54fa
urotansh <114655455+urotansh@users.noreply.github.com>
2022-12-26 06:28:01 +0000

    [Add] コメント機能 コメント投稿機能の実装(非同期通信)

--- app/controllers/public/note_comments_controller.rb
+++ app/controllers/public/note_comments_controller.rb
@@ -1,7 +1,26 @@
 class Public::NoteCommentsController < ApplicationController
   def create
+    @note_comment = NoteComment.new(note_comment_params)
+    @note_comment.user_id = current_user.id
+    @note_comment.note_id = params[:note_id]
+    
+    if @note_comment.save
+      # *.js.erbで参照するインスタンス
+      @note = Note.find(params[:note_id])
+      render "create"
+    else
+      render "error"
+    end
+      
   end
 
   def destroy
   end
+  
+  private
+  
+  def note_comment_params
+    params.require(:note_comment).permit(:comment)
+  end
+  
 end
