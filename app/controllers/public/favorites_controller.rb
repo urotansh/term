d5b28bfd262b15b3073291a3833aa20592b3d89a
urotansh <114655455+urotansh@users.noreply.github.com>
2022-12-26 10:22:14 +0000

    [Add] いいね機能 いいね追加機能の実装(非同期通信)

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -1,5 +1,10 @@
 class Public::FavoritesController < ApplicationController
   def create
+    favorite = Favorite.new(user_id: current_user.id, note_id: params[:note_id])
+    favorite.save
+    
+    # *.js.erbで参照するインスタンス
+    @note = Note.find(params[:note_id])
   end
 
   def destroy
