952d468c11aed4904adc1b33a859ef5e71b6183e
urotansh <114655455+urotansh@users.noreply.github.com>
2022-12-26 08:01:20 +0000

    [Add] コメント機能 コメント削除機能の実装(非同期通信)

--- app/controllers/public/note_comments_controller.rb
+++ app/controllers/public/note_comments_controller.rb
@@ -15,6 +15,14 @@ class Public::NoteCommentsController < ApplicationController
   end
 
   def destroy
+    @note_comment = NoteComment.find(params[:id])
+    if @note_comment.destroy
+      # *.js.erbで参照するインスタンス
+      @note = Note.find(params[:note_id])
+      render "destroy"
+    else
+      render "error"
+    end
   end
   
   private
