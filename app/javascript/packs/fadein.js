7352eb6fc3fc1d1ebfc2b1fcc55651a67ddc8db8
urotansh <114655455+urotansh@users.noreply.github.com>
2023-01-15 15:45:16 +0000

    [Fix] コメント機能 コメント投稿時のフェードイン表示を抑制

--- app/javascript/packs/fadein.js
+++ app/javascript/packs/fadein.js
@@ -3,6 +3,16 @@ import $ from 'jquery';
 // 投稿内容をフェードインで表示
 'ajax:complete turbolinks:load'.split(' ').forEach((eventName)=>{
   document.addEventListener(eventName, (e)=>{
+    if (document.getElementById("comment_post_flg") != null){
+      return;
+    }
     $("#note").hide().fadeIn();
+  });
+});
+
+// jsTreeをフェードインで表示
+'turbolinks:load'.split(' ').forEach((eventName)=>{
+  document.addEventListener(eventName, (e)=>{
+    $("#jstree").hide().fadeIn();
   });
 });
