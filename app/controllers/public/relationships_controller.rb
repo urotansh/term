c963021d5ca124cb0291fa10711858d033772127
urotansh <114655455+urotansh@users.noreply.github.com>
2023-03-20 14:19:15 +0000

    [Add] フォロー機能 フォロー/アンフォロー機能の実装

--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -1,7 +1,18 @@
 class Public::RelationshipsController < ApplicationController
+  before_action :set_user, only: [:create, :destroy]
+  
   def create
+    current_user.follow(@user)
   end
 
   def destroy
+    current_user.unfollow(@user)
   end
+  
+  private
+  
+  def set_user
+    @user = User.find_by(name: params[:user_name])
+  end
+  
 end
