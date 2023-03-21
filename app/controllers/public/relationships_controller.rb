f983d5a751a95a7fdf1d3561ad2a07ab584758dd
urotansh <114655455+urotansh@users.noreply.github.com>
2023-03-21 09:54:34 +0000

    [Improve] フォロー機能 パスパラメータ(ユーザー名)を変数へ格納
    https://github.com/rails/rails/blob/f2d1b7bbc758ee7fe611ea3429629b250931770f/actionpack/lib/action_dispatch/routing/route_set.rb#L865-L878

--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -1,5 +1,6 @@
 class Public::RelationshipsController < ApplicationController
   before_action :set_user, only: [:create, :destroy]
+  before_action :set_path_param_name, only: [:create, :destroy]
   
   def create
     current_user.follow(@user)
@@ -13,6 +14,16 @@ class Public::RelationshipsController < ApplicationController
   
   def set_user
     @user = User.find_by(name: params[:user_name])
+  end
+  
+  def set_path_param_name
+    # パスパラメータ(ユーザー名)を変数へ格納
+    # Rails.application.routes.recognize_path(path, environment = {})
+    # => コントローラー, アクション, パスパラメータをハッシュで返却
+    # => {:controller=>"public/users", :action=>"show", :name=>"test1"}
+    # request.referrer
+    # => リクエスト元URL
+    @path_param_name = Rails.application.routes.recognize_path(request.referrer)[:name]
   end
   
 end
